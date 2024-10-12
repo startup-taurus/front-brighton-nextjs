46bcfd1a4a8e4f4e09fcf0a3b6037a862957aa0c
RichardGarciaP <richardgarcia2499@gmail.com>
2024-10-12 17:21:25 -0500

    comment middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,15 +3,15 @@ import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  if (
-    path.split("/")[1] !== "authentication" &&
-    !request.cookies.has("token")
-  ) {
-    return NextResponse.redirect(new URL("/authentication/login", request.url));
-  }
-  if (path.split("/")[1] === "authentication" && request.cookies.has("token")) {
-    return NextResponse.redirect(new URL(`/teachers`, request.url));
-  }
+  // if (
+  //   path.split("/")[1] !== "authentication" &&
+  //   !request.cookies.has("token")
+  // ) {
+  //   return NextResponse.redirect(new URL("/authentication/login", request.url));
+  // }
+  // if (path.split("/")[1] === "authentication" && request.cookies.has("token")) {
+  //   return NextResponse.redirect(new URL(`/teachers`, request.url));
+  // }
 }
 
 export const config = {
