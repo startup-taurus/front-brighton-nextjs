9198c567a699a32c40d871f4f94fae1c89c2be7f
RichardGarciaP <68579638+RichardGarciaP@users.noreply.github.com>
2024-10-24 14:55:38 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   env: {
-    //API_URL: "http://localhost:3000/v1/api",
+    //API_URL: "http://localhost:3000/v1/api" //Dev,
     API_URL: "http://154.12.252.34:3000/v1/api",    
   },
   redirects: async () => {
