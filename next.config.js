1c2ee4d87a6dae15b21b7446cbba06281bde35c8
maxrd1995 <57199275+maxrd1995@users.noreply.github.com>
2024-10-24 14:44:58 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const nextConfig = {
   env: {
-    API_URL: "http://localhost:3000/v1/api",
+    //API_URL: "http://localhost:3000/v1/api",
+    API_URL: "http://154.12.252.34:3000/v1/api",    
   },
   redirects: async () => {
     return [
