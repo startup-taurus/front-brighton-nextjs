c4232a15086ac678133d246efc2a911baca9f1f1
Jean Santos <138512979+jpsantosc79@users.noreply.github.com>
2025-01-04 14:26:31 -0500

    feat: update syllabus form

--- src/components/own/form/syllabus-form.tsx
+++ src/components/own/form/syllabus-form.tsx
@@ -53,6 +53,9 @@ const SyllabusForm = ({ data, isOpen, toggle }: any) => {
                   test_percentage: data?.percentages?.test_percentage || 0,
                   exam_percentage: data?.percentages?.exam_percentage || 0,
                   assig_percentage: data?.percentages?.assig_percentage || 0,
+                  assignments: data?.assignments || [""],
+                  progress_tests: data?.progress_tests || [""],
+                  movers_exam: data?.movers_exam || [""],
                 }
               : {
                   id: "",
@@ -61,6 +64,9 @@ const SyllabusForm = ({ data, isOpen, toggle }: any) => {
                   test_percentage: 0,
                   exam_percentage: 0,
                   assig_percentage: 0,
+                  assignments: [""],
+                  progress_tests: [""],
+                  movers_exam: [""],
                 }
           }
           onSubmit={(values) => (data ? update(values) : save(values))}
@@ -74,26 +80,16 @@ const SyllabusForm = ({ data, isOpen, toggle }: any) => {
               setFieldValue,
             } = props;
 
-            return (
-              <form
-                noValidate
-                autoComplete="off"
-                onSubmit={handleSubmit}
-                className={`row g-3`}
-              >
-                <Col xs={12}>
-                  <Label for="syllabus_name">Syllabus Name</Label>
-                  <Field name="syllabus_name" as={Input} />
-                  <ErrorMessage name="syllabus_name" component={FormFeedback} />
-                </Col>
-                <Col xs={12} className="mt-3">
-                  <Label>Items</Label>
-                  <FieldArray
-                    name="items"
-                    render={(arrayHelpers) => (
-                      <div style={{ maxHeight: "300px", overflowY: "auto" }}>
-                        <Row>
-                          {values.items.map((item: string, index: number) => (
+            const renderArrayField = (name: string, label: string) => (
+              <Col xs={12} className="mt-3">
+                <Label>{label}</Label>
+                <FieldArray
+                  name={name}
+                  render={(arrayHelpers) => (
+                    <div style={{ maxHeight: "300px", overflowY: "auto" }}>
+                      <Row>
+                        {(values[name as keyof typeof values] as string[]).map(
+                          (item, index) => (
                             <Col
                               key={index}
                               xs={6}
@@ -103,7 +99,7 @@ const SyllabusForm = ({ data, isOpen, toggle }: any) => {
                                 value={item}
                                 onChange={(e) =>
                                   setFieldValue(
-                                    `items[${index}]`,
+                                    `${name}[${index}]`,
                                     e.target.value
                                   )
                                 }
@@ -118,19 +114,37 @@ const SyllabusForm = ({ data, isOpen, toggle }: any) => {
                                 <FaTrash />
                               </Button>
                             </Col>
-                          ))}
-                        </Row>
-                        <Button
-                          type="button"
-                          color="primary"
-                          onClick={() => arrayHelpers.push("")}
-                        >
-                          Add Item
-                        </Button>
-                      </div>
-                    )}
-                  />
+                          )
+                        )}
+                      </Row>
+                      <Button
+                        type="button"
+                        color="primary"
+                        onClick={() => arrayHelpers.push("")}
+                      >
+                        Add Item
+                      </Button>
+                    </div>
+                  )}
+                />
+              </Col>
+            );
+
+            return (
+              <form
+                noValidate
+                autoComplete="off"
+                onSubmit={handleSubmit}
+                className={`row g-3`}
+              >
+                <Col xs={12}>
+                  <Label for="syllabus_name">Syllabus Name</Label>
+                  <Field name="syllabus_name" as={Input} />
+                  <ErrorMessage name="syllabus_name" component={FormFeedback} />
                 </Col>
+
+                {renderArrayField("items", "Items")}
+
                 <Col md={4}>
                   <Label for="assig_percentage">Assignment Percentage %</Label>
                   <Field name="assig_percentage" as={Input} type="number" />
@@ -155,6 +169,12 @@ const SyllabusForm = ({ data, isOpen, toggle }: any) => {
                     component={FormFeedback}
                   />
                 </Col>
+                <hr />
+                {renderArrayField("assignments", "Assignments")}
+                <hr />
+                {renderArrayField("progress_tests", "Progress Tests")}
+                <hr />
+                {renderArrayField("movers_exam", "Movers Exam")}
                 <Col xs={12} className="d-flex justify-content-end mt-5">
                   <Button color="cancel" onClick={toggle}>
                     Close
