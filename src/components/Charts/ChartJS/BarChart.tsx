21513b9b8242c1a7eda28c2aefbf63bb73903cc6
RichardGarciaP <richardgarcia@zgamestechnology.com>
2025-03-04 16:37:23 -0500

    fix: merge

--- src/components/Charts/ChartJS/BarChart.tsx
+++ src/components/Charts/ChartJS/BarChart.tsx
@@ -1,20 +1,17 @@
-import { Bar } from 'react-chartjs-2';
-import { Col, Card, CardBody } from 'reactstrap';
-import { BarChart } from 'utils/Constant';
-import { barChartData, barChartOptions } from 'Data/Charts/ChartJsData';
-import CommonCardHeading from 'CommonElements/CommonCardHeading';
-
+import { Bar } from "react-chartjs-2";
+import { Col, Card, CardBody } from "reactstrap";
+import { BarChart } from "utils/Constant";
+import { barChartData } from "Data/Charts/ChartJsData";
+import CommonCardHeading from "CommonElements/CommonCardHeading";
 
 const BarChartClass = () => {
   return (
-      <Col xl={6} md={12} className='box-col-12'>
-        <Card>
+    <Col xl={6} md={12} className="box-col-12">
+      <Card>
         <CommonCardHeading smallHeading={BarChart} />
-          <CardBody className="chart-block">
-            <Bar data={barChartData} options={barChartOptions} width={778} height={400} />
-          </CardBody>
-        </Card>
-      </Col>
+        <CardBody className="chart-block"></CardBody>
+      </Card>
+    </Col>
   );
 };
 
