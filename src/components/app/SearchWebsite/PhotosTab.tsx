c01ee92ae2ce3fbce97659afa956e4c4b845503e
MiltonAngamarca <miltonangamarca@zgamestechnology.com>
2025-04-03 13:03:53 -0500

    refactor(PhotosTab): remove unused GalleryImageDescription component
    The GalleryImageDescription component was imported but not utilized in the PhotosTab component. This change removes the unused import and its corresponding JSX elements to clean up the code and improve maintainability.

--- src/components/app/SearchWebsite/PhotosTab.tsx
+++ src/components/app/SearchWebsite/PhotosTab.tsx
@@ -1,16 +1,13 @@
 import { ImageAbout } from 'utils/Constant';
 import { Row } from 'reactstrap';
 import PagesSort from './Pages';
-import GalleryImageDescription from '@/components/Miscellaneous/gallery/GalleryGridWithDescription/GalleryImageDescription';
 
 const PhotosTab = () => {
   return (
     <>
       <div>
         <h6 className='mb-2 col-sm-12 digits'>{ImageAbout}</h6>
-        <Row className='my-gallery gallery-with-description'>
-          <GalleryImageDescription />
-        </Row>
+        <Row className='my-gallery gallery-with-description'></Row>
       </div>
       <PagesSort />
     </>
