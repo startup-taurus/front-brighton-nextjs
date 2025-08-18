8a3a1ceb865132a80c468819516afce444b57a0f
Marcoprozgames <marcoproano@zgamestechnology.com>
2025-08-18 10:44:05 -0500

    fix: modify table course

--- src/components/own/tables/courses-table.tsx
+++ src/components/own/tables/courses-table.tsx
@@ -132,7 +132,7 @@ const CoursesTable = ({ reload }: any) => {
     },
     {
       name: 'Classroom',
-      selector: (row: any) => `${row.classroom}`.toLocaleUpperCase(),
+      selector: (row: any) => row.classroom ? `${row.classroom}`.toLocaleUpperCase() : '',
       sortable: true,
       center: false,
     },
